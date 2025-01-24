8860e1667a3e4e0281cc9f249f8ad8747869d5f2
Ximena Burbano <daburbanoo@unal.edu.co>
2025-01-23 19:52:05 -0500

    07_matplotlib_plot

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,15 +4,137 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
 def pregunta_01():
     """
     Siga las instrucciones del video https://youtu.be/qVdwpxG_JpE para
-    generar el archivo `files/plots/news.png`.
+    generar el archivo files/plots/news.png.
 
     Un ejemplo de la grafica final esta ubicado en la raíz de
     este repo.
 
-    El gráfico debe salvarse al archivo `files/plots/news.png`.
+    El gráfico debe salvarse al archivo files/plots/news.png.
 
     """
+    #Carga de datos
+    def load_data(input_file):
+        df=pd.read_csv(input_file, index_col=0)
+        return df
+    
+
+    def plot_data(df):
+        #crea una figura vacía
+        plt.figure()
+
+        #lectura
+        #df= pd.read_csv("news.csv", index_col=0)
+        
+        colors= {
+            "Television": "dimgray",
+            "Newspaper": "grey",
+            "Internet": "tab:blue",
+            "Radio": "Lightgrey"
+        }
+
+        zorder= {
+            'Television': 1,
+            'Newspaper': 1,
+            'Internet': 2,
+            'Radio': 1,
+        }
+
+        linewidths={
+            'Television':2,
+            'Newspaper': 2,
+            'Internet': 4,
+            'Radio': 2,       
+        }
+
+        # Graficar líneas
+        for col in df.columns:
+            plt.plot(
+                df[col],
+                color=colors[col],
+                label=col,
+                zorder=zorder[col],
+                linewidth=linewidths[col],
+            )   
+        
+        # Configuración de título y ejes
+        plt.title("How people get their news", fontsize=16)
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['left'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().axes.get_yaxis().set_visible(False)
+
+        # Añadir etiquetas y puntos al principio y al final de cada línea
+        for col in df.columns:
+            first_year=df.index[0]
+            plt.scatter(
+                x=first_year,
+                y=df[col][first_year],
+                color=colors[col],
+                zorder=zorder[col],
+            )
+
+        # Punto y etiqueta en el primer año
+            plt.text(
+                first_year-0.2,
+                df[col][first_year],
+                col + " " + str(df[col][first_year]) + "%",
+                ha='right',
+                va='center',
+                color=colors[col],
+            )
+
+            last_year=df.index[-1]
+            plt.scatter(
+                x=last_year,
+                y=df[col][last_year],
+                color=colors[col],
+            )
+
+        # Punto y etiqueta en el último año
+            plt.text(
+                last_year + 0.2,
+                df[col][last_year],
+                str(df[col][last_year]) + "%",
+                ha='left',
+                va='center',
+                color=colors[col],
+            )
+
+        # Configuración de los ticks del eje x
+        plt.xticks(
+            ticks=df.index,
+            labels=df.index,
+            ha='center',
+        )
+
+    # Guardar imágen
+    def save_data(df, output_dir):
+        # Crear el directorio si no existe
+        output_dir = os.path.dirname(output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        plt.tight_layout()
+        plt.savefig(os.path.join(output_dir, "news.png")) #antes plt.savefig(output_dir,"news.png")
+        #plt.show()
+
+    def main(input_file, output_dir):
+        df= load_data(input_file)
+        plot_data(df)
+        save_data(df, output_dir)
+
+    main(
+        input_file="files/input/news.csv",
+        output_dir="files/plots/" 
+    )
+    return "Imagen generada"
+if __name__ == "__main__":
+    
+    print(pregunta_01())
